ed066ff73163f244a18e971fbfa60a4bfca69632
Bit Barron <1bit.baron@gmail.com>
2026-09-25 11:56:56 +0200

    Load credentials from environment

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    STRIPE_TOKEN: "sk_test_51N3PcuF8j0Y1AkmM4TYh7joCr4sTLHTtHPXAknqSOxENkFpL0B4PMdefhwQ3Z2TiuvER0QnWX3UgK1iUjyFSgLcD00oaExbJz4",
-    CONSUMER_SECRET: "cs_bdf157192fabf79150c73daddf867d106db2c13b",
-    CONSUMER_KEY: "ck_9296be97c44ed317137dc18944e29f2bfbf74500"
+    STRIPE_TOKEN: process.env.STRIPE_TOKEN,
+    CONSUMER_SECRET: process.env.CONSUMER_SECRET,
+    CONSUMER_KEY: process.env.CONSUMER_KEY,
   },
   output: "standalone",
   images: {
